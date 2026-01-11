6e0679a013d52341766b4587e459fe52509f5fdc
chikeluba <modoziechikeluba6@gmail.com>
2026-01-11 05:16:29 +0100

    feat: complete exercise 2 - stock manager with error handling
    - Implemented dictionary structure to store stock prices as lists
    - Added 'print' functionality to calculate and display average prices
    - Added 'add' functionality to append to existing stocks or create new ones
    - Implemented error handling (try/except) for invalid user inputs
    - Added support for comma-separated price inputs

--- pyBasics/11_dict_tuples.py
+++ pyBasics/11_dict_tuples.py
@@ -63,3 +63,53 @@ elif command == "query":
 
 else:
     print("Invalid command!")
+
+
+# 2. You are given following list of stocks and their prices in last 3 days,
+# Stock	Prices
+# info	[600,630,620]
+# ril	[1430,1490,1567]
+# mtl	[234,180,160]
+# i. Write a program that asks user for operation. Value of operations could be,
+#       a. print: When user enters print it should print following,
+#       info == > [600, 630, 620] == > avg: 616.67
+#       ril == > [1430, 1490, 1567] == > avg: 1495.67
+#       mtl == > [234, 180, 160] == > avg: 191.33
+#
+#       b. add: When user enters 'add', it asks for stock ticker and price. If stock already exist in your list (like info, ril etc) then it will append the price to the list. Otherwise it will create new entry in your dictionary. For example entering 'tata' and 560 will add tata ==> [560] to the dictionary of stocks.
+
+# SOLUTION
+stocks = {
+    "info" : [600, 630, 620],
+    "rill" : [1430, 1490, 1567],
+    "mtl"  : [234, 180, 160]
+}
+operation_input = input("Enter operation (print, add): ").lower()
+
+#   ---OPERATION A: PRINT---
+if operation_input == "print":
+    for stock, price in stocks.items():
+        # calculate avg
+        avg_price = sum(price) / len(price)
+        print(f"{stock.lower()} == > {price} == > avg: {avg_price}")
+
+#   ---OPERATION B: ADD---
+elif operation_input == "add":
+    stock_to_add = input("Enter a stock name to add: ").lower()
+    if stock_to_add in stocks:
+        append_to_list = int(input(f"stock already exists for {stock_to_add}. Add a price to the list: "))
+        stocks[stock_to_add].append(append_to_list)
+        print(f"New price {append_to_list} added to Stocks. \n {stocks} ")
+    else:
+        raw_input = input(f"Enter price for {stock_to_add} separated by comma:")
+        try:
+            price_list_string = raw_input.split(",")
+            final_price_list = [int(price) for price in price_list_string]
+            stocks[stock_to_add] = final_price_list
+            print("Stock added!")
+            print(stocks)
+        except ValueError:
+            print("Please make sure you use commas and only enter numbers (no letters!).")
+
+else:
+    print("Invalid command!")
